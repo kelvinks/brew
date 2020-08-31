1678c4d7a0ad53262dd20b9732109d3cb7ba6356
Kelvin Kang <kji9623@gmail.com>
2020-08-31 10:34:24 +0900

    Update setting.sh
    find some errors, so update sh

--- setting.sh
+++ setting.sh
@@ -34,6 +34,7 @@ echo '/usr/local/bin/zsh Added OK!'
 chsh -s `which zsh`
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 git clone https://github.com/powerline/fonts.git && ./fonts/install.sh
+git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom/}/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/alias-tips
