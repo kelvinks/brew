52757c654dc91c6c45c7727291d3911685a276f4
Kelvin Kang <kji9623@gmail.com>
2020-11-25 00:15:44 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -2,7 +2,7 @@
 ## kelvink.ga (kji9623@gmail.com)
 ## 2020-08-21
 
-cat <<EOT > /etc/resolv.conf
+sudo cat <<EOT > /etc/resolv.conf
 # Google DNS
 nameserver 8.8.8.8
 nameserver 8.8.4.4
@@ -15,32 +15,36 @@ nameserver 8.8.4.4
 ;nameserver 180.182.54.1
 ;nameserver 210.94.0.73
 EOT
-echo 'Google DNS Changed OK!'
+echo '>>> Google DNS Changed OK!'
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/bundle
-echo 'Homebrew Installed OK!'
+echo '>>> Homebrew Installed OK!'
 
-echo 'Brewfile Package Installing..'
+brew install curl
+brew install wget
+echo '>>> Homebrew Base Apps Installed OK!'
+
+echo '>>> Brewfile Package Installing..'
 cd $HOME/Downloads/ && wget BrewFile https://raw.githubusercontent.com/kelvinks/brew/master/Brewfile
 brew bundle Brewfile
-echo 'Brewfile Package Installed OK!'
+echo '>>> Brewfile Package Installed OK!'
 
-cat <<EOT >> /etc/shells
+sudo cat <<EOT >> /etc/shells
 /usr/local/bin/zsh
 EOT
-echo '/usr/local/bin/zsh Added OK!'
+echo '>>> /usr/local/bin/zsh Added OK!'
 
-chsh -s `which zsh`
+sudo chsh -s `which zsh`
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 git clone https://github.com/powerline/fonts.git && ./fonts/install.sh
 git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom/}/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/alias-tips
-echo 'zsh Installed OK!'
+echo '>>> zsh Installed OK!'
 
 cd $HOME && mv .zshrc .zshrc_backup
 wget https://raw.githubusercontent.com/kelvinks/brew/master/.zshrc
-echo 'oh-my-zsh Installed OK!'
+echo '>>> oh-my-zsh Installed OK!'
 source ~/.zshrc
