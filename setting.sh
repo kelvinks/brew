3accdc23b7e92d405513bd94800c6782dd215f41
Kelvin Kang <kji9623@gmail.com>
2020-12-13 00:45:09 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,50 +1,37 @@
 #!/bin/sh
 ## kelvink.ga (kji9623@gmail.com)
-## 2020-08-21
-
-sudo cat <<EOT > /etc/resolv.conf
-# Google DNS
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-
-# KT
-;nameserver 168.126.63.1
-;nameserver 168.126.63.2
-
-# LG Uplus
-;nameserver 180.182.54.1
-;nameserver 210.94.0.73
-EOT
-echo '>>> Google DNS Changed OK!'
+## 2020-12-12
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/bundle
 echo '>>> Homebrew Installed OK!'
 
+brew tap homebrew/bundle
 brew install curl
 brew install wget
 echo '>>> Homebrew Base Apps Installed OK!'
 
-echo '>>> Brewfile Package Installing..'
-cd $HOME/Downloads/ && wget BrewFile https://raw.githubusercontent.com/kelvinks/brew/master/Brewfile
+cd $HOME
+wget Brewfile https://raw.githubusercontent.com/kelvinks/brew/master/Brewfile
 brew bundle Brewfile
 echo '>>> Brewfile Package Installed OK!'
 
-sudo cat <<EOT >> /etc/shells
-/usr/local/bin/zsh
-EOT
-echo '>>> /usr/local/bin/zsh Added OK!'
-
-sudo chsh -s `which zsh`
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-git clone https://github.com/powerline/fonts.git && ./fonts/install.sh
-git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom/}/themes/powerlevel9k
+echo '>>> Oh-My-Zsh Installed OK!'
+
+git clone https://github.com/powerline/fonts.git
+./fonts/install.sh
+echo '>>> Powerline Fonts Install OK!'
+
+cd $HOME
+git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/alias-tips
-echo '>>> zsh Installed OK!'
+echo '>>> Oh-My-Zsh Plugins OK!'
 
-cd $HOME && mv .zshrc .zshrc_backup
+cd $HOME
+mv .zshrc .zshrc_backup
 wget https://raw.githubusercontent.com/kelvinks/brew/master/.zshrc
-echo '>>> oh-my-zsh Installed OK!'
+echo '>>> Oh-My-Zsh Config OK!'
+
 source ~/.zshrc
